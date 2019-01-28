d1dfe2c5446348c4b07a667af2096172b743600f
zaticpetru <zatic.petru@outlook.com>
2019-01-28 17:06:14 +0000

    fixed stocks paths

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  
   def self.new_from_lookup(ticker_symbol)
     begin 
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
