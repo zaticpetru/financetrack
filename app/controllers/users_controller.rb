034ff56be874fcc07014ca1f408cda994cb64846
zaticpetru <zatic.petru@outlook.com>
2019-01-29 13:51:21 +0000

    add remove friend functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,35 @@
 class UsersController < ApplicationController
- def my_portfolio
+  def my_portfolio
   @user = current_user
   @user_stocks = current_user.stocks
- end
- 
- def my_friends
+  end
+  
+  def my_friends
   @friendships = current_user.friends
- end
+  end
  
   def search
     if params[:search_param].blank?
       flash.now[:danger] = "You have entered empty search string"
     else
       @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
       flash.now[:danger] = "No users match this search criteria" if @users.blank?
     end
     respond_to do |format|
       format.js { render partial: 'friends/result' }
     end
+  end
+  
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    
+    if current_user.save
+      flash[:success] = "Friend was successfully added"
+    else
+      flash[:danger] = "There was something wrong with the friend request"
+    end
+    redirect_to my_friends_path
   end
 end
