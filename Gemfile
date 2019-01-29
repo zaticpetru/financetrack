f395f0769097c44e54326bb148653ff01dd83f60
zaticpetru <zatic.petru@outlook.com>
2019-01-29 10:28:54 +0000

    added friendship model

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jquery-rails'
 gem 'devise-bootstrap-views'
 
 gem 'stock_quote'
+
+gem 'font-awesome-rails'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
   resources :user_stocks, only: [:create, :destroy]
 end
